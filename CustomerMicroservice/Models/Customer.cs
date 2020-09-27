70903bec0a025ee25e783dc209afac4212cc9fef
Siddhartha Banerjee <sbanjee.banerjee@gmail.com>
2020-09-27 18:27:42 +0530

    Customer code(not unit test) added

---
+++ CustomerMicroservice/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerMicroservice.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string DOB { get; set; }
+        public string PanNo { get; set; }
+    }
+}
---
+++ CustomerMicroservice/Models/CustomerCreationStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerMicroservice.Models
+{
+    public class CustomerCreationStatus
+    {
+        public string Message { get; set; }
+        public int CustomerId { get; set; }
+        public int CurrentAccountId { get; set; }
+        public int SavingsAccountId { get; set; }
+    }
+}
