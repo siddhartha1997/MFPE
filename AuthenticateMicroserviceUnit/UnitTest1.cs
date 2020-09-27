127a8996b46d25441f05cc31082c80c1fb7cec6e
Siddhartha Banerjee <sbanjee.banerjee@gmail.com>
2020-09-27 18:25:29 +0530

    Authentication Code Added

---
+++ AuthenticateMicroserviceUnit/UnitTest1.cs
@@ -0,0 +1,37 @@
+using AuthenticateMicroservice.Controllers;
+using AuthenticateMicroservice;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using AuthenticateMicroservice.Model;
+using AuthenticateMicroservice.Repository;
+
+namespace AuthenticateMicroserviceUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsTokenNotNullIsTokenNotNull_When_ValidUserCredentialsAreUsed()
+        {
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+            var TokenObj = new TokenRep(config.Object);
+            var Result = TokenObj.AuthenticateUser(new Authenticate { UserID = 1, Password = "1234", Roles = "Employee" });
+            Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNull_When_InvalidUserCredentialsAreUsed()
+        {
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+            var TokenObj = new TokenRep(config.Object);
+            var Result = TokenObj.AuthenticateUser(new Authenticate() { UserID = 1, Password = "12345",Roles="Employee" });
+            Assert.IsNull(Result);
+        }
+    }
+}
