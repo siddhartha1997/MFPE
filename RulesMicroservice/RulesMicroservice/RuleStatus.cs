5bc903a883e69931ade964525b136416c70be545
Sen <856737@cognizant.com>
2020-09-28 12:15:27 +0530

    rules added

---
+++ RulesMicroservice/RulesMicroservice/RuleStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RulesMicroservice
+{
+    public class RuleStatus
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ RulesMicroservice/RulesMicroservice/RulesMsg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RulesMicroservice
+{
+    public class RulesMsg
+    {
+        public int AccId { get; set; }
+        public double AccBal { get; set; }
+        public string Message { get; set; }
+    }
+}
